232dc64616872e5008e2204b62e37c53fa6d185b
B-JThomas <bailey.jthomas2@gmail.com>
2024-05-28 21:47:07 +1000

    Header/Footer Contained in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,62 @@
-import React from 'react';
-import {BrowserRouter, Routes, Route } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css';
 
+//=====COMPONENTS=====
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+//=====PAGES=====
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NoPage from './pages/NoPage';
 
+
 function App() {
+
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const checkAuthenticated = async () => {
+    try {
+      const res = await fetch("http://localhost:5001/auth/verify/", {
+        method: "GET",
+        headers: { token: localStorage.token }
+      });
+  
+      const parseData = await res.json();
+      setAuthenticated(parseData === true);
+      console.log("Condition: " + parseData);
+
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    checkAuthenticated();
+  }, []);
+
+
   return (
     <div>
+      {authenticated ? (<Header />) : <></>}
       <BrowserRouter >
         <Routes>
-          <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          {!authenticated ? (
+            <>
+              <Route index element={<Login />} />
+              <Route path="/login" element={<Login />} />
+            </>
+          ) : (
+            <>
+              <Route index element={<Home />} />
+              <Route path="/home" element={<Home />} />
+            </>
+          )}
           <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
+      <Footer />
     </div>
   );
 }
--- frontend/src/pages/NoPage.js
+++ frontend/src/pages/NoPage.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
 
 const NoPage = () => {
   return (
     <div>
-      <Header />
       <h1>Error 404 Not Found</h1>
-
-      <Footer />
     </div>
   )
 }
